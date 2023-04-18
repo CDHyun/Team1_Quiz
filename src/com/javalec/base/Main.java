dd5e7c1cbfd5677dc6e419b0466e67109f0bbf65
Rian <pdchoirian@gmail.com>
2023-04-18 18:02:05 +0900

    main method 기본 틀 구현

--- src/com/javalec/base/Main.java
+++ src/com/javalec/base/Main.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		 - 문제 풀이는 Constructor로 함.
+		 
+		 
+		 */
 		
 		Scanner scanner = new Scanner(System.in);
 		int firstNum = 0;
 		int secondNum = 0;
 		int act = 0;
+		boolean loop = true;
 		
 		System.out.print("첫번째 숫자를 입력하세요 : ");
 		firstNum = scanner.nextInt();
@@ -21,7 +27,28 @@ public class Main {
 		System.out.println("1. 덧셈\n2. 뺄셈\n3. 곱셈\n4. 나눗셈\n5. 몫과 나머지\n6. 종료");
 		act = scanner.nextInt();
 		
-		// 나 지금 되는거니??
+		// 각 case에서 기능 메소드를 불러서 구현되게 제작.
+		
+		
+		while(loop == true) {
+			switch(act){
+				case 1:
+				
+				case 2:
+					
+				case 3:
+					
+				case 4:
+					
+				case 5:
+					
+				case 6:
+					System.out.println(">>> Thank You <<<");
+					loop = false;
+					return;
+			}
+		}
+		
 		
 	}
 
