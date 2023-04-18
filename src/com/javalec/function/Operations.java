c9bdc745713d8bf964762c376b1ea0cb99ff0ccc
jinnie <solia0394@naver.com>
2023-04-18 18:04:32 +0900

    operation 틀 제작

--- src/com/javalec/function/Operations.java
+++ src/com/javalec/function/Operations.java
@@ -1,5 +1,48 @@
 package com.javalec.function;
 
 public class Operations {
+	
+	// Field
+	private int firstNum;
+	private int secondNum;
+	private int act;
+	int result ;
+	double divResult ;
+	double reminder;
+	
+	// Constructor 
+	public Operations() {
+		// TODO Auto-generated constructor stub
+	}
+	public Operations(int firstNum, int secondNum, int act) {
+		super();
+		this.firstNum = firstNum;
+		this.secondNum = secondNum;
+		this.act = act;
+	}
 
+	// Method 
+	public int addCalc () {
+		result = firstNum + secondNum ;
+		return result;
+	}
+	public int subCalc () {
+
+	}
+	public int multiCalc () {
+
+	}
+	public double divCalc () {
+
+	}
+	
+	public double reminderCalc() { 		// 나머지 
+		reminder = firstNum % secondNum ;
+		return reminder;
+	}
+	
+	
+	
+	
+	
 }
