4f652fe647183aaa505f5f2202bb9ce316aa029f
Rian <pdchoirian@gmail.com>
2023-04-18 23:45:46 +0900

    몫과 나머지 구하기 기능 구현 Project 오타 수정 Comment 추가 Main Class 구현 및 Test 완료.

--- src/com/javalec/base/Main.java
+++ src/com/javalec/base/Main.java
@@ -9,15 +9,13 @@ public class Main {
 	public static void main(String[] args) {
 		/*
 		 - 문제 풀이는 Constructor로 함.
-		 
-		 
 		 */
 		
 		Scanner scanner = new Scanner(System.in);
-		int firstNum = 0;
-		int secondNum = 0;
-		int act = 0;
-		boolean loop = true;
+		int firstNum = 0;				// 사용자가 입력하는 첫번째 숫자
+		int secondNum = 0;				// 사용자가 입력하는 두번째 숫자
+		int act = 0;					// 사용자가 입력하는 행동
+		boolean loop = true;			// while문 반복 여부
 		
 		System.out.print("첫번째 숫자를 입력하세요 : ");
 		firstNum = scanner.nextInt();
@@ -25,37 +23,40 @@ public class Main {
 		System.out.print("두번째 숫자를 입력하세요 : ");
 		secondNum = scanner.nextInt();
 		
-		System.out.println("*** 원하는 결과를 선택하세요 ***");
-		System.out.println("1. 덧셈\n2. 뺄셈\n3. 곱셈\n4. 나눗셈\n5. 몫과 나머지\n6. 종료");
-		System.out.print("==========> 선택 : ");
-		act = scanner.nextInt();
 		
 		// 각 case에서 기능 메소드를 불러서 구현되게 제작.
+		// Main에서 출력문 없이 출력되게 제작.
 		Operations operations = new Operations(firstNum, secondNum);
 		
-		
+		// loop가 true일 경우, while문은 무한히 반복됨.
 		while(loop == true) {
+			System.out.println("*** 원하는 결과를 선택하세요 ***");
+			System.out.println("1. 덧셈\n2. 뺄셈\n3. 곱셈\n4. 나눗셈\n5. 몫과 나머지\n6. 종료");
+			System.out.print("==========> 선택 : ");
+			act = scanner.nextInt();
 			switch(act){
-				case 1:
+				case 1:	// 덧셈
 					operations.addCalc();
 					break;
-				case 2:
-					
+				case 2:	// 뺄셈
+					operations.subCalc();
 					break;
-				case 3:
-					operations.addCalc();
+				case 3:	// 곱셈
+					operations.multiCalc();
 					break;
-				case 4:
-					
+				case 4:	// 나눗셈
 					operations.divCalc();
 					break;
-				case 5:
-					
+				case 5:	// 몫과 나머지
+					operations.remainderCalc();
 					break;
-				case 6:
-					System.out.println(">>> Thank You <<<");
-					loop = false;
-					return;
+				case 6:	// 종료
+					System.out.println("계산 프로그램을 종료합니다. 감사합니다.");
+					loop = false;		// 사용자가 종료를 실행시, loop가 false로 바뀌며 while문 반복 종료.
+					break;
+				default : // default(잘못된 Case)
+					System.out.println("잘못된 번호를 선택하셨습니다. 올바른 번호를 입력해주세요.");
+					break;
 			}
 		}
 		
--- src/com/javalec/function/Operations.java
+++ src/com/javalec/function/Operations.java
@@ -5,11 +5,10 @@ public class Operations {
 	// Field
 	private int firstNum;
 	private int secondNum;
-	private int act;
 	int result;
 	int quotient;
 	double divResult ;
-	double reminder;
+	double remainder;
 	
 	// Constructor 
 	public Operations() {
@@ -24,33 +23,42 @@ public class Operations {
 
 
 
-	// Method 
+	// Method ----
+	// 더하기 기능 Method
 	public int addCalc () {
 		result = firstNum + secondNum;
-		System.out.println(firstNum + " + " +secondNum + " = " + result);
+		System.out.println(firstNum + " + " + secondNum + " = " + result);
 		return 0;
 	}
-//	public int subCalc () {
+	
+	// 빼기 기능 Method
+	public int subCalc () {
 		result = firstNum - secondNum;
-		System.out.println(firstNum + " - " +secondNum + " = " + result);
+		System.out.println(firstNum + " - " + secondNum + " = " + result);
 		return 0;
 	}
+	
+	// 곱하기 기능 Method
 	public int multiCalc () {
 		result = firstNum * secondNum ;
-		return result;
-//
+		System.out.println(firstNum + " X " + secondNum + " = " + result);
+		return 0;
 	}
+	
+	// 나누기 기능 Method
 	public double divCalc () {
 		divResult = (double)firstNum / secondNum;
-		System.out.println(firstNum + " / " +secondNum + " = " + divResult);
+		System.out.println(firstNum + " ÷ " + secondNum + " = " + String.format("%.2f", divResult));
 		return 0;
 		
 	}
 	
-	public double reminderCalc() { 		// 몫과 나머지 
-		reminder = firstNum % secondNum;
-		quotient = firstNum / secondNum;
-		return reminder;
+	// 몫과 나머지
+	public double remainderCalc() {
+		quotient = firstNum / secondNum;		//몫
+		remainder = firstNum % secondNum;		//나머지
+		System.out.println(firstNum + " ÷ " + secondNum + "의 몫은 " + quotient + "이고 나머지는 " + remainder + "입니다.");
+		return 0;
 	}
 	
 	
